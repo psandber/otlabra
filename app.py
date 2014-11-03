bd5c9fa24aba90717663bbbaec456a14fbe07204
Petrik Mikael Sandberg <petriksandberg@gmail.com>
2014-11-03 14:12:16 +0200

    FizzBuzz muokattu

--- app.py
+++ app.py
@@ -22,7 +22,14 @@ class FizzBuzz():
 
     # Seems to give correct values. Tested with 1 and 2.
     def calc(self, i):
-        return i
+        if (i%3 == 0 and i%5 == 0):
+            return "FizzBuzz"
+        elif (i%3 == 0):
+            return "Fizz"
+        elif (i%5 == 0):
+            return "Buzz"
+        else:
+            return i
 
 if __name__ == "__main__":
     app = FizzBuzz()
